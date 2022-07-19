3009e801d12543adeffac9de2c2a0c9cae9dfef3
louisinayinde <lousinayinde@gmail.com>
2022-07-19 14:41:37 +0200

    closing feature navbar

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,5 +1,7 @@
 import React from 'react'
-import { Box, Stack, Typography } from '@mui/material'
+import { Box, Stack, Typography, Button } from '@mui/material'
+
+import HeroBannerImage from '../assets/images/banner.png'
 
 const HeroBanner = () => {
   return (
@@ -16,12 +18,40 @@ const HeroBanner = () => {
       >
         Fitness Club
       </Typography>
-      <Typography>
+      <Typography
+        fontWeight="700"
+        sx={{ fontSize: {
+          lg: '44px', xs: '40px'
+        }}}
+        mb="23px"
+        mt="30px"
+      >
         Sweat, Smile <br /> and Repeat
       </Typography>
-      <Typography>
+      <Typography fontSize="22px"
+        lineHeight="35px"
+        mb={4}
+      >
         Check out the most effective exercises
       </Typography>
+      <Button variant="contained" color="error"
+        sx={{
+          backgroundColor: '#ff2625',
+          padding: '10px'
+        }}
+      >Explore Exercises</Button>
+      <Typography
+        fontWeight="600"
+        color="#ff2625"
+        sx={{
+          opacity: 0.1 ,
+          display: {lg : 'block', xs: 'none'}
+        }}
+        fontSize="20px"
+      >
+        Exercise
+      </Typography>
+      <img src={HeroBannerImage} alt="banner" className="hero-banner-img" />
     </Box>
   )
 }
