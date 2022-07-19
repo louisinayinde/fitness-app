1d23c51470a7ff464c744f5cb4b5fa47d57772ce
louisinayinde <lousinayinde@gmail.com>
2022-07-19 14:15:01 +0200

    beginning navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Stack } from '@mui/material'
+
+import Logo from '../assets/images/Logo.png'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <Stack>
+      <Link to="/">
+        <img src={Logo} alt="logo" style={{width: '48px', height: '48px', margin: '0 20px'}}/>
+      </Link>
+      <Stack>
+        <Link to="/" style={{textDecoration: 'none', color: "#3a1212", borderBottom: '3px solid #ff2625'}}>Home</Link>
+        <a href="#exercises" style={{textDecoration: 'none', color: '#3a1212'}}>Exercises </a>
+      </Stack>
+    </Stack>
   )
 }
 
